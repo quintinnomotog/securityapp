2961f2f8307c0360703aae79b3d7c8e86e9496d6
José Quintinno <josequintino@hotmail.com.br>
2025-06-11 12:35:29 -0300

    bugfix: Correção do comportamento do ícone de Visualizar a Senha

--- src/app/screen/signup/signup.page.ts
+++ src/app/screen/signup/signup.page.ts
@@ -1,24 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonIcon, IonLabel, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonIcon,
+  IonLabel,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { arrowBackOutline, eyeOffOutline } from 'ionicons/icons';
+import { arrowBackOutline, eyeOffOutline, eyeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonFooter, IonLabel, IonToolbar, IonHeader, IonIcon, IonButton, IonButtons, IonContent, CommonModule, FormsModule]
+  imports: [
+    IonFooter,
+    IonLabel,
+    IonToolbar,
+    IonHeader,
+    IonIcon,
+    IonButton,
+    IonButtons,
+    IonContent,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class SignupPage implements OnInit {
+  public ICONE: string = 'eye-off-outline';
+
+  public TYPE_INPUT: string = 'password';
 
   constructor() {
-    addIcons({arrowBackOutline,eyeOffOutline});
+    addIcons({ arrowBackOutline, eyeOffOutline, eyeOutline });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  public showPassword() {
+    if (this.TYPE_INPUT === 'password') {
+      this.TYPE_INPUT = 'text';
+      this.ICONE = 'eye-outline';
+    } else {
+      this.TYPE_INPUT = 'password';
+      this.ICONE = 'eye-off-outline';
+    }
+  }
 }
